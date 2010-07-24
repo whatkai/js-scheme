ca578ef31452fec439b27efc38920b6e644248ec
eriksilkensen <eriksilkensen@4e7f2476-bf53-0410-a708-3dafa0ef0028>
2010-07-24 18:52:53 +0000

    using Google AJAX API for prototype

--- js-scheme.js
+++ js-scheme.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  JS-SCHEME - a Scheme interpreter written in JavaScript
- (c) 2008 Erik Silkensen, erik@silkensen.com, version 0.4
+ (c) 2010 Erik Silkensen, eriksilkensen@gmail.com, version 0.4
 
  This program is free software: you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
